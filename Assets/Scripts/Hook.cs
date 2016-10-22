49aee890bd0b68cc2c880664f838f49c62025e25
haruto0kitune <haruto0kitune@gmail.com>
2016-10-22 19:24:23 +0900

    Add HookShot

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class Hook : MonoBehaviour 
+{
+	[System.NonSerialized]
+	public Rigidbody2D rigidbody2d;
+	[System.NonSerialized]
+	public SpringJoint2D springJoint2d;
+	[System.NonSerialized]
+	public SpriteRenderer spriteRenderer;
+	[System.NonSerialized]
+	public BoxCollider2D boxCollider2d;
+	[System.NonSerialized]
+	public bool isStopping;
+	
+	
+    void Awake ()
+    {
+	    rigidbody2d = GetComponent<Rigidbody2D>();
+	    springJoint2d = GetComponent<SpringJoint2D>();
+	    spriteRenderer = GetComponent<SpriteRenderer>();
+	    boxCollider2d = GetComponent<BoxCollider2D>();
+    }
+
+    void Start () 
+	{
+		// Stop on stuck hookable object
+	    this.OnTriggerEnter2DAsObservable()
+		    .Where(x => x.gameObject.tag == "Hookable")
+		    .Subscribe(_ => Stop());
+    }
+	
+	void Stop()
+	{
+		rigidbody2d.isKinematic = false;
+		rigidbody2d.velocity = Vector2.zero;
+		rigidbody2d.constraints = RigidbodyConstraints2D.FreezeAll;
+		springJoint2d.enabled = true;
+		isStopping = true;
+	}
+}
